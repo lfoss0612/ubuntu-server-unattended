f07d0e1bbcdb8c70653d81cda580a335577132ca
Lester Foss <lfoss0612@gmail.com>
2021-03-16 02:58:01 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,12 +19,26 @@ do
   raidPathMap[$path]=${raidSerialMap[$serial]}
 done < <(lsblk -r -o PATH,SERIAL|grep -e "$serialGrepStr")
 
-sudo mkdir /raid1
-
 for key in "${!raidPathMap[@]}"
 do
-sudo mkdir /raid1/${raidPathMap[$key]}
-echo $key
+sudo mkdir -p /raid1/${raidPathMap[$key]}
 sudo mount "$key" "/raid1/${raidPathMap[$key]}"
 done
 
+sudo apt-get -y update && sudo apt-get -y upgrade 
+sudo apt-get install gcc make samba
+
+sudo mkdir /var/lib/snapraid 
+sudo chmod a+w /var/lib/snapraid 
+cd /var/lib/snapraid
+
+wget https://github.com/amadvance/snapraid/releases/download/v11.5/snapraid-11.5.tar.gz 
+tar -xzf snapraid-11.5.tar.gz 
+cd snapraid-11.5
+./configure 
+
+make 
+make check 
+sudo make install 
+
+cd ~ && rm /var/lib/snapraid/snapraid-11.5.tar.gz
