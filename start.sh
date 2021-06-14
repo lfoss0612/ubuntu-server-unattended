b9c13c2bb15707c301916f475c1b45d8e39a2aa5
Lester Foss <lfoss0612@gmail.com>
2021-06-14 02:20:37 +0000

    Added Samba Config

--- start.sh
+++ start.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+setup_samba () {
+
+cp /etc/samba/smb.conf /etc/samba/smb.conf.orig
+touch  /etc/samba/includes.conf
+echo "include = /etc/samba/includes.conf" >> /etc/samba/smb.conf
+mkdir /etc/samba/includes
+cp smb.conf /etc/samba/includes/drive_share_mappings.conf
+ls /etc/samba/includes/* | sed -e 's/^/include = /' > /etc/samba/includes.conf
+service smbd restart
+
+}
+
+setup_snapraid () {
+
+mkdir -p /config/snapraid
+cp *.conf /config/snapraid
+
+useradd snapraid
+docker pull xagaba/snapraid
+
+}
+
+build_drive_mappings () {
+
 declare -A raidSerialMap
 serialGrepStr=""
 
@@ -7,7 +31,7 @@ while IFS="|" read -r serial label
 do
   raidSerialMap[$serial]=$label
   serialGrepStr+="$serial\|"
-done < raid1_mappings.txt
+done < $1
 
 len=${#serialGrepStr}
 serialGrepStr=${serialGrepStr::len-2}
@@ -20,30 +44,53 @@ done < <(lsblk -r -o PATH,SERIAL|grep -e "$serialGrepStr")
 
 for key in "${!raidPathMap[@]}"
 do
-mkdir -p /raid1/${raidPathMap[$key]}
-mount "$key" "/raid1/${raidPathMap[$key]}"
+mkdir -p /$2/${raidPathMap[$key]}
+mount "$key" "/$2/${raidPathMap[$key]}"
 done
+
+}
+
+
+prereq_docker () {
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+	sudo gpg --dearmor /usr/share/keyrings/docker-archive-keyring.gpg
+
+}
+
+prereq_webmin () {
+
+wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb http://download.webmin.com/download/repository sarge contrib"
+
+}
+
+
+
+build_drive_mappings raid1_mappings.txt raid1
 
 apt-get -y update && sudo apt-get -y upgrade 
 apt-get install \
 	samba \
+        smbclient \
+        software-properties-common \
 	apt-transport-https \
 	ca-certificates \
 	curl \
-	gnupg lsb-release
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-	sudo gpg --dearmor /usr/share/keyrings/docker-archive-keyring.gpg
+	gnupg lsb-release \
+        cockpit \
+        net-tools 
+        
+prereq_docker
+prereq_webmin
 
 apt-get -y update
 sudo apt-get install \
 	docker-ce \
 	docker-ce-cli \
-	containerd.io
+	containerd.io \
+        webmin
 
+setup_snapraid
+setup_samba
 
-mkdir -p /config/snapraid
-cp *.conf /config/snapraid
-
-useradd snapraid
-docker pull xagaba/snapraid
